b6ff8b0e6d15dda53bf6dd1de756cd0e5033f842
Richard Bogdan <richard.bogdan@bitrise.io>
2021-01-27 14:13:29 +0100

    chore: Documentation update
    Added more documentation.
    
    APM-1911

--- configuration.go
+++ configuration.go
@@ -1,5 +1,7 @@
 package main
 
+// These configurations should be constant (final and static), code should access them accordingly. The reason for this
+// is that this file should hold them, to make it easy to have a clear overview and make it the single source of truth.
 const configFileName = "bitrise-addons-configuration.json"
 const configFileVersion = "1.0.0"
 const apmTokenEnvName = "APM_COLLECTOR_TOKEN"
